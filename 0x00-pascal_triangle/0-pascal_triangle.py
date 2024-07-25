976e5fbb67b4417e835c0e4938a89974b9e0a5a4
Sylvester009 <sasy260902@gmail.com>
2024-07-25 15:33:40 +0100

    revision on pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,14 +1,14 @@
 # function for pascal triangle
 def pascal_triangle(n):
     # check if n is lesser than 0
-    if n < 0:
+    if n <= 0:
         return []
     
     # initialize triangle by creating the first row
     triangle = [[1]]
     
     # iterate through all the row
-    for i in range(0, n):
+    for i in range(1, n):
         # initialize start for
         # every first digit in a row
         start = [1]
@@ -16,9 +16,9 @@ def pascal_triangle(n):
         # fill up the middle of the row
         for j in range(1, i):
             start.append(triangle[i-1][j-1] + triangle[i-1][j])
-            start.append(1)
+        start.append(1)
             
-            # add the row to the triangle
-            # and return it
-            triangle.append(start)
+        # add the row to the triangle
+        # and return it
+        triangle.append(start)
     return triangle
