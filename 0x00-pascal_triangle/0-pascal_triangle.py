b391f2575ed9e2c39339b1a40a1837a8e3f54db8
Sylvester009 <sasy260902@gmail.com>
2024-07-25 16:06:12 +0100

    revision on pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,25 +4,25 @@
  function for pascal triangle
 """
 def pascal_triangle(n):
-    # check if n is lesser than 0
+    # check if n is less than or equal to 0
     if n <= 0:
         return []
-
-    # initialize triangle by creating the first row
-    triangle = [[1]]
-
-    # iterate through all the row
-    for i in range(1, n):
-        # initialize start for
-        # every first digit in a row
-        start = [1]
-
-        # fill up the middle of the row
-        for j in range(1, i):
-            start.append(triangle[i-1][j-1] + triangle[i-1][j])
-        start.append(1)
-
+ 
+    # initialize the triangle
+    triangle = []
+ 
+    # first row is always 1  
+    for i in range(n):
+        # initialize the row
+        row = []
+        C = 1  # starting value for the binomial coefficient
+     
+        # fill the row with binomial coefficients
+        for j in range(i + 1):
+            row.append(C)
+            C = C * (i - j) // (j + 1)
+  
         # add the row to the triangle
-        # and return it
-        triangle.append(start)
+        triangle.append(row)
+
     return triangle
