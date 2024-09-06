c40e77dfac2f7a4c7e9d92c5d68ce7ad8ea54ad8
Samuel Sylvester <samsylvester09@gmail.com>
2024-09-06 01:24:00 +0100

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+// Import 'request' module for making HTTP request
 const request = require('request');
 
 // Function to make request as a promise
@@ -11,6 +12,7 @@ const requestPromise = (url) => {
       } else if (response.statusCode !== 200) {
         reject(`Status code: ${response.statusCode}`);
       } else {
+        // Parse JSON
         resolve(JSON.parse(body));
       }
     });
@@ -21,14 +23,17 @@ const requestPromise = (url) => {
 const movieId = process.argv[2];
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
+// Fetch the movie data
 requestPromise(apiUrl)
   .then((data) => {
     const characters = data.characters;
 
+    // Make requests for all characters in parallel using Promise.all
     return Promise.all(
       characters.map((url) => requestPromise(url))
     );
   })
+  // Extract and print character names
   .then((characterData) => {
     characterData.forEach((character) => {
       console.log(character.name);
