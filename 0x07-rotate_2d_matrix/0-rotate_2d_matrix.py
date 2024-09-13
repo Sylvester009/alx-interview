654b40e88b4c49a6028f20e5faf0d44cd760b9a9
Samuel Sylvester <samsylvester09@gmail.com>
2024-09-13 03:36:00 +0100

    Update 0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,12 +3,13 @@
 Rotate 2D Matrix
 """
 
+
 def rotate_2d_matrix(matrix):
     """Rotates a 2D matrix 90 degrees clockwise in place
     without returning anything.
     """
     n = len(matrix)
-    
+
     # Loop over each layer, starting from the outer layer, inwards
     for i in range(n // 2):
         # Define the last index of the current layer
@@ -16,7 +17,7 @@ def rotate_2d_matrix(matrix):
         # Iterate over each element in the layer
         for x in range(i, k):
             y = n - 1 - x
-            # Perform the 4-way rotation swap
+            # Perform  4-way rotation swap
             tmp = matrix[i][x]
             matrix[i][x] = matrix[y][i]
             matrix[y][i] = matrix[k][y]
