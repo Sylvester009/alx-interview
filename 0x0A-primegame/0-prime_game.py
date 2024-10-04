ce2181861c3215ab743a630c432bd6614e5d696d
Samuel Sylvester <samsylvester09@gmail.com>
2024-10-04 02:47:54 +0100

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,47 +1,45 @@
 #!/usr/bin/python3
-
-""" Python program to print all
-primes smaller than or equal to
-n using Sieve of Eratosthenes
-"""
-
-
-def SieveOfEratosthenes(n):
-    """Returns a list of booleans indicating
-    whether numbers are prime up to n"""
-    prime = [True for _ in range(n+1)]
-    p = 2
-    while (p * p <= n):
-        if prime[p]:
-            for i in range(p * p, n+1, p):
-                prime[i] = False
-        p += 1
-    prime[0], prime[1] = False, False
-    return prime
-
-
-"""
-Determine the winner of the Prime Game after x rounds
-"""
+"""Game involving the determining of winner
+of a game based on the strategic removal
+of prime numbers and their multiples from
+a set of consecutive integers."""
 
 
 def isWinner(x, nums):
-    if not nums or x < 1:
+    """Determines the winner of the Prime Game.
+    
+    Args:
+        x: Number of rounds.
+        nums: List of 'n' values for each round.
+        
+    Returns:
+        The name of the player who won the most rounds.
+        If no clear winner, return None.
+    """
+    
+    if x <= 0 or nums is None or x != len(nums):
         return None
 
-    max_nums = max(nums)
-    prime = SieveOfEratosthenes(max_nums)
-
-    maria_wins = 0
     ben_wins = 0
+    maria_wins = 0
 
-    for n in nums:
-        primes_count = sum(prime[2:n+1])
-        if primes_count % 2 == 0:
+    max_nums = max(nums)
+    sieve = [True] * (max_nums + 1)
+    sieve[0] = sieve[1] = False
+
+    for i in range(2, int(max_nums ** 0.5) + 1):
+        if sieve[i]:
+            for j in range(i * i, max_nums + 1, i):
+                sieve[j] = False
+
+    for num in nums:
+        prime_count = sum(sieve[2:num + 1])
+        if prime_count % 2 == 0:
             ben_wins += 1
         else:
             maria_wins += 1
 
+    # Determine the overall winner
     if maria_wins > ben_wins:
         return "Maria"
     elif ben_wins > maria_wins:
